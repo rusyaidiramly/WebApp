114f8013dd88603f5e20f852f604d635bff6dbe5
Rusyaidi <rusyaidiramly@gmail.com>
2022-04-09 14:50:37 +0800

    Maps controllers on startup, added constructor on controller accepting JsonFileService instance

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using RestAPI.Models;
+using RestAPI.Services;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/User/{id}/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        public static readonly string fileName = "MessageList.json";
+        private List<Message> Messages;
+        public JsonFileService _jsonService;
+
+        public MessageController(JsonFileService jsonService)
+        {
+            _jsonService = jsonService;
+            Messages = _jsonService.LoadJsonFile<Message>(fileName);
+        }
+
+        [HttpGet]
+        public IEnumerable<Message> Get()
+        {
+            return Messages.FindAll(
+                    message => message.AuthorID == int.Parse(RouteData.Values["id"].ToString())
+            );
+        }
+
+        [HttpGet("{mId}")]
+        public Message Get(string mId)
+        {
+            return Messages.Find(message
+                    => message.MessageID == mId
+                    && message.AuthorID == int.Parse(RouteData.Values["id"].ToString())
+            );
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Message value)
+        {
+            Messages.Add(new Message
+            {
+                MessageID = RndService.RndAlphaNum(8),
+                AuthorID = int.Parse(RouteData.Values["id"].ToString()),
+                PlainMessage = value.PlainMessage
+            });
+
+            _jsonService.SaveJsonFile(Messages, fileName);
+        }
+
+        [HttpPut("{mId}")]
+        public void Put(string mId, [FromBody] Message value)
+        {
+            Message selectedMessage = Messages.Find(message
+                                        => message.MessageID == mId
+                                        && message.AuthorID == int.Parse(RouteData.Values["id"].ToString())
+                                    );
+
+            if (selectedMessage == null) return;
+            if (value.PlainMessage != null) selectedMessage.PlainMessage = value.PlainMessage;
+
+            _jsonService.SaveJsonFile(Messages, fileName);
+
+        }
+
+        [HttpDelete("{mId}")]
+        public void Delete(string mId)
+        {
+            Message selectedMessage = Messages.Find(message
+                                        => message.MessageID == mId
+                                        && message.AuthorID == int.Parse(RouteData.Values["id"].ToString())
+                                    );
+
+            if (selectedMessage == null) return;
+            Messages.Remove(selectedMessage);
+
+            _jsonService.SaveJsonFile(Messages, fileName);
+        }
+    }
+}
---
+++ Controllers/UserContoller.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using RestAPI.Models;
+using RestAPI.Services;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public static readonly string fileName = "UserList.json";
+        private List<User> Users;
+        public JsonFileService _jsonService;
+
+        public UserController(JsonFileService jsonService)
+        {
+            _jsonService = jsonService;
+            Users = _jsonService.LoadJsonFile<User>(fileName);
+        }
+
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return Users;
+        }
+
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return Users.Find(user => user.UserID == id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] User value)
+        {
+            int currentID = (Users?.Any() == true) ? Users.Last().UserID : 0;
+            Users.Add(new User
+            {
+                UserID = currentID + 1,
+                Name = value.Name,
+                Email = value.Email,
+                Password = value.Password,
+                NRIC = value.NRIC,
+                DOB = value.DOB
+            });
+
+            _jsonService.SaveJsonFile(Users, fileName);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] User value)
+        {
+            User selectedUser = Users.Find(user => user.UserID == id);
+            if (selectedUser == null) return;
+            if (value.Name != null) selectedUser.Name = value.Name;
+            if (value.Email != null) selectedUser.Email = value.Email;
+            if (value.Password != null) selectedUser.Password = value.Password;
+            if (value.NRIC != null) selectedUser.NRIC = value.NRIC;
+            if (value.NRIC == null && value.DOB != null) selectedUser.DOB = value.DOB;
+
+            _jsonService.SaveJsonFile(Users, fileName);
+
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            User selectedUser = Users.Find(user => user.UserID == id);
+            if (selectedUser == null) return;
+            Users.Remove(selectedUser);
+
+            _jsonService.SaveJsonFile(Users, fileName);
+        }
+    }
+}
