4eedfcece05b1707d6a38c3602dde4bf08013bd1
Rusyaidi <rusyaidiramly@gmail.com>
2022-04-09 13:49:32 +0800

    Added JsonFIleService param on constructor, loop thru model Users

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,21 +5,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestAPI.Models;
+using RestAPI.Services;
 
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileService _jsonService;
+        public List<User> Users { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileService jsonService)
         {
             _logger = logger;
+            _jsonService = jsonService;
         }
 
         public void OnGet()
         {
-
+            Users = _jsonService.LoadJsonFile<User>("UserList.json");
         }
     }
 }
