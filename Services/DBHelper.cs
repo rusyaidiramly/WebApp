9d2d8c13ee731a2a3517f6cfe44c4c4ed5dd9d84
Rusyaidi <rusyaidiramly@gmail.com>
2022-04-11 20:24:52 +0800

    Added class DBHelper for type check cast from database

---
+++ Services/DBHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WebApp.Services
+{
+    public class DBHelper
+    {
+        public static T ConvertFromDBVal<T>(object obj)
+        {
+            if (obj == null || obj == DBNull.Value)
+            {
+                return default(T);
+            }
+            else
+            {
+                return (T)obj;
+            }
+        }
+    }
+}
